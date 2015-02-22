1671c965e04aac1374195e4e42ef1b8c66733252
James Gao <james@lyra>
2015-02-21 20:47:58 -0800

    add ds18b20 reading to firmware

--- firmware/controller/controller.ino
+++ firmware/controller/controller.ino
@@ -3,7 +3,7 @@
 #define PIN_STEP2     8
 #define PIN_STEP3     7
 #define PIN_STEP4     6
-#define PIN_AUXTEMP   A1
+#define PIN_AUXTEMP   2
 #define PIN_TEMP_CS   4
 #define PIN_LOADCELL  A3
 #define PIN_FLAME_A   A2
@@ -12,8 +12,17 @@
 
 #define STEP_SPEED 275//in steps per second
 #define TEMP_UPDATE 250 //milliseconds
-#define MOTOR_TIMEOUT 5000 //milliseconds
+#define AUX_UPDATE 1000 //milliseconds
+#define MOTOR_TIMEOUT 60000 //milliseconds
 
+#define NUM_AUXTEMP 2
+
+#define NO_PORTB_PINCHANGES
+#define NO_PORTC_PINCHANGES
+#define DISABLE_PCINT_MULTI_SERVICE
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <Stepper.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -28,7 +37,7 @@ struct Status {
   float main_temp;
   float ambient;
   float weight;
-  float aux_temp[2];
+  float aux_temp[NUM_AUXTEMP];
 } status;
 uint8_t* status_data = (uint8_t*) &status;
 
@@ -36,22 +45,34 @@ const float step_interval = 1. / STEP_SPEED * 1000.; //milliseconds
 
 //intermediate variables
 Adafruit_MAX31855 thermo(PIN_TEMP_CS);
-Stepper stepper(2048, PIN_STEP1, PIN_STEP3, PIN_STEP2, PIN_STEP4);
+Stepper stepper(2048, PIN_STEP4, PIN_STEP2, PIN_STEP3, PIN_STEP1);
+pushbutton reglimit = pushbutton(PIN_REGLIMIT, 5);
+OneWire oneWire(PIN_AUXTEMP);
+DallasTemperature sensors(&oneWire);
+DeviceAddress aux_addr[NUM_AUXTEMP];
 
 char i2c_command;
 float next_step;
 unsigned long next_temp;
+unsigned long next_aux = 0;
 unsigned char motor_active = false;
 unsigned long stepper_target = 0;
-unsigned int n_clicks = 0; //Number of full rotations
+unsigned long num_aux = 0;
+int n_clicks = 0; //Number of full rotations
 boolean limit_state = false;
 unsigned long limit_last;
 
 void setup() {
+  //setup ignition mosfet
+  pinMode(PIN_IGNITE, OUTPUT);
+  digitalWrite(PIN_IGNITE, LOW);
+  status.ignite = 0.;
+  
   status.flame = false;
   status.weight = 0.;
-  status.aux_temp[0] = 0.;
-  status.aux_temp[1] = 0.;
+  status.aux_temp[0] = -1.;
+  status.aux_temp[1] = -1.;
+  sensors.begin();
   
   //Setup I2C
   Wire.begin(0x08);
@@ -61,33 +82,32 @@ void setup() {
   //Set up regulator stepper
   status.motor = 0;
   
-  //setup regulator limit switch
-  pinMode(PIN_REGLIMIT, INPUT);
-  digitalWrite(PIN_REGLIMIT, HIGH);
-  
-  //setup ignition mosfet
-  pinMode(PIN_IGNITE, OUTPUT);
-  digitalWrite(PIN_IGNITE, LOW);
-  status.ignite = false;
-  
-  //set initial temperature
+  //set initial thermocouple temperature
   delay(500);
   update_temp();
   next_temp = millis() + TEMP_UPDATE;
+  
+  //Setup auxtemp ds18b20 sensors
+  num_aux = sensors.getDeviceCount();
+  num_aux = NUM_AUXTEMP < num_aux ? NUM_AUXTEMP : num_aux;
+  for (int i = 0; i < num_aux; i++) {
+    sensors.getAddress(aux_addr[i], i);
+    sensors.setResolution(aux_addr[i], 12);
+    status.aux_temp[i] = 0.;
+  }
+  if (num_aux > 0) {
+    sensors.setWaitForConversion(false);
+    sensors.requestTemperatures();
+    next_aux = millis() + AUX_UPDATE;
+  }
 }
 
 int dir;
 unsigned long now;
 void loop() {
   now = millis();
-  if (digitalRead(PIN_REGLIMIT) == LOW) {
-    if (limit_last == 0) {
-      limit_last = millis();
-    } else if ((millis() - limit_last) > 5) {
-      n_clicks += dir;
-      limit_last = 0;
-    }
-  }
+  reglimit.update();
+  status.aux_temp[1] = reglimit.n_clicks;
   
   if (stepper_target != status.motor && now > next_step) {
     dir = status.motor < stepper_target ? 1 : -1;
@@ -95,7 +115,7 @@ void loop() {
     
     //Limit switch tripped
     if (stepper_target == 0) {
-      if (n_clicks == 0)
+      if (reglimit.n_clicks == 0)
         status.motor = 0;
     } else {
       status.motor += dir;
@@ -119,6 +139,12 @@ void loop() {
     next_temp += TEMP_UPDATE;
   }
   
+  //update auxtemp
+  if (num_aux > 0 && now > next_aux) {
+    update_aux();
+    next_aux += AUX_UPDATE;
+  }
+  
   //check flame status
 }
 
@@ -134,9 +160,14 @@ void update_temp() {
   thermo.readAll(status.main_temp, status.ambient);
 }
 
+void update_aux() {
+  for (int i = 0; i < num_aux; i++) {
+    status.aux_temp[i] = sensors.getTempC(aux_addr[i]);
+  }
+  sensors.requestTemperatures();
+}
+
 void i2c_update() {
-  //update temperatures
-  
   if (i2c_command == 'M') {
     Wire.write((byte*) &(status.motor), 4);
   } else if (i2c_command == 'I') {
@@ -170,7 +201,8 @@ void i2c_action(int nbytes) {
       set_regulator(*((unsigned int*) buffer));
       break;
     case 'I':
-      digitalWrite(PIN_IGNITE, buffer[0]);
+      analogWrite(PIN_IGNITE, buffer[0]);
+      status.ignite = buffer[0];
       break;
   }
   
