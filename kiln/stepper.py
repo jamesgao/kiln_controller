b6ae241f0852fb50fcf6d29a6f07d92c73a5197a
James Gao <james@kiln>
2015-02-22 20:48:52 +0000

    Lower limits for stepper

--- kiln/stepper.py
+++ kiln/stepper.py
@@ -244,7 +244,7 @@ class Regulator(threading.Thread):
         pass
 
 class Breakout(object):
-    def __init__(self, addr, maxsteps=6500, minsteps=((2900, 0), (2480, 12)) ):
+    def __init__(self, addr, maxsteps=6500, minsteps=((2700, 0), (2400, 15)) ):
         import breakout
         self.device = breakout.Breakout(addr)
         self.min_interp = minsteps
